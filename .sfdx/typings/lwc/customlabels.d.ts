4afbc66a62b897bfb298bb6655ce6eb6d20efa54
Adam Rodenbeck <adrodenbeck@gmail.com>
2019-08-05 18:42:44 -0700

    Update from surfaceBook

---
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -0,0 +1,28 @@
+declare module "@salesforce/label/c.fflib_security_error_field_not_insertable" {
+    var fflib_security_error_field_not_insertable: string;
+    export default fflib_security_error_field_not_insertable;
+}
+declare module "@salesforce/label/c.fflib_security_error_field_not_readable" {
+    var fflib_security_error_field_not_readable: string;
+    export default fflib_security_error_field_not_readable;
+}
+declare module "@salesforce/label/c.fflib_security_error_field_not_updateable" {
+    var fflib_security_error_field_not_updateable: string;
+    export default fflib_security_error_field_not_updateable;
+}
+declare module "@salesforce/label/c.fflib_security_error_object_not_deletable" {
+    var fflib_security_error_object_not_deletable: string;
+    export default fflib_security_error_object_not_deletable;
+}
+declare module "@salesforce/label/c.fflib_security_error_object_not_insertable" {
+    var fflib_security_error_object_not_insertable: string;
+    export default fflib_security_error_object_not_insertable;
+}
+declare module "@salesforce/label/c.fflib_security_error_object_not_readable" {
+    var fflib_security_error_object_not_readable: string;
+    export default fflib_security_error_object_not_readable;
+}
+declare module "@salesforce/label/c.fflib_security_error_object_not_updateable" {
+    var fflib_security_error_object_not_updateable: string;
+    export default fflib_security_error_object_not_updateable;
+}
